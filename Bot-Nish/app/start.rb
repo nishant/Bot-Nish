86511888e1bd97bf1496a596b11e4e9df62fdec2
Nishant Arora <nish54@gmail.com>
2019-08-03 20:44:27 -0400

    base project structure

---
+++ Bot-Nish/app/start.rb
@@ -0,0 +1,18 @@
+# TODO:
+#  sunset bug
+#  send file when too many cards to list
+
+# requires
+require 'open-uri'
+require 'json'
+require 'time'
+require_relative 'lib/secrets.rb'
+require_relative 'help.rb'
+require_relative 'misc.rb'
+require_relative 'weather.rb'
+require_relative 'stock.rb'
+require_relative 'fortnite.rb'
+require_relative 'math.rb'
+require_relative 'yugioh.rb'
+require_relative 'media.rb'
+require_relative 'generate.rb'

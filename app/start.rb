68a34d2b5632f3fd0dde2100ce3a6afa8e026edc
Nishant Arora <nish54@gmail.com>
2019-08-06 20:28:49 -0400

    added gitignore

--- app/start.rb
+++ app/start.rb
@@ -1,18 +1,15 @@
 # TODO:
 #  sunset bug
-#  send file when too many cards to list
+#  too many cards to list -> send as file
 
-# requires
+require 'bundler/setup'
 require 'open-uri'
 require 'json'
 require 'time'
-require_relative 'lib/secrets.rb'
-require_relative 'help.rb'
-require_relative 'misc.rb'
-require_relative 'weather.rb'
-require_relative 'stock.rb'
-require_relative 'fortnite.rb'
-require_relative 'math.rb'
-require_relative 'yugioh.rb'
-require_relative 'media.rb'
-require_relative 'generate.rb'
+
+Bundler.require :default
+Dir.glob('lib/*.rb') { |f| require_relative f }
+
+
+
+

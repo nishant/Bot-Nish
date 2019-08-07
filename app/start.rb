ada13f0689cf5ec471db15f90e45a0ae9c811f6d
Nishant Arora <nish54@gmail.com>
2019-08-06 21:22:03 -0400

    added start file

--- app/start.rb
+++ app/start.rb
@@ -10,6 +10,24 @@ require 'time'
 Bundler.require :default
 Dir.glob('lib/*.rb') { |f| require_relative f }
 
+bot = Discordrb::Commands::CommandBot.new(
+    token: get_discord_token(),
+    client_id: get_discord_id,
+    prefix: '%'
+)
 
+time = Time.now
 
+process_help(bot)
+process_misc(bot, time)
+process_weather(bot)
+process_stock(bot)
+process_weather(bot)
+process_fn(bot)
+process_math(bot)
+process_yugioh(bot)
+# process_spotify(bot)
+process_generate(bot)
+
+bot.run
 
